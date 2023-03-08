e93905aaecdc816a592e9dee2e7df507f7141081
edpadua <ed.padua@gmail.com>
2023-03-08 10:23:58 -0300

    Implementação do componente responsável por mostrar os dados de uma receita
    Implementação do componente que exibe as informações e conteúdo de uma receita

--- receitas/src/Componentes/Receita/index.js
+++ receitas/src/Componentes/Receita/index.js
@@ -2,10 +2,32 @@ import React from 'react'
 
 import './Receita.css';
 
-function Receitas(){
+
+
+
+function Receita({receita}){
     return(
-        <div>
-            <h2>Receita</h2>
+        <div className="Receita">
+            <img className="imagem" src={receita.recipe.image} alt=""/>
+            <h2>{receita.recipe.label}</h2>
+             <ol className='ingredientes'>
+                {receita.recipe.ingredients.map(ingrediente=>(
+                    <li className='ingrediente'>{ingrediente.text}</li>
+                ))}
+             </ol>
+
+             <ul className='ingredientes'>
+                {receita.recipe.cuisineType.map(tipoDeCulinaria=>(
+                    <li className='ingrediente'>{tipoDeCulinaria}</li>
+                ))}
+             </ul>
+
+            <p>Calorias : {receita.recipe.calories}</p>
+
+           
+
+            <a href={receita.recipe.url} target="_blank" rel="noopener noreferrer"><button>Instruções</button></a>
+
         </div>
         
     )
--- receitas/src/Componentes/Receitas/index.js
+++ receitas/src/Componentes/Receitas/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
+import Receita from '../Receita';
+
 import './Receitas.css';
 
 import axios from "axios";
@@ -7,7 +9,7 @@ import axios from "axios";
 function Receitas() {
     const APP_ID = process.env.REACT_APP_API_ID;
     const APP_KEY = process.env.REACT_APP_API_KEY;
-    
+
 
     const [receitas, setReceitas] = useState([]);
     const [busca, setBusca] = useState("");
@@ -25,19 +27,29 @@ function Receitas() {
 
     const atualizaBusca = e => {
         setBusca(e.target.value);
-      }
-    
+    }
+
 
 
     const getReceitas = async () => {
         const res = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-        setReceitas(res.data);
-        console.log(res.data);
+        setReceitas(res.data.hits);
+        console.log(res.data.hits);
+        
     }
 
+
+   /* const getReceitas = async () => {
+        const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data = await response.json();
+        setReceitas(data.hits);
+        console.log(data.hits);
+    }*/
+
+
     return (
-        <div>
-            <h2>Receitas</h2>
+        <div className="receitasContainer">
+            <h2 className="receitasContainerTitulo">Receitas</h2>
 
             <form onSubmit={getBusca} className="busca-form">
 
@@ -46,7 +58,20 @@ function Receitas() {
             </form>
 
             <div className="receitas">
+                {console.log("receitas", receitas)}
 
+               
+                    {
+                    receitas.map(receita => (
+                        <div>
+                            <Receita
+                                receita={receita}
+                                
+                            />
+                        </div>
+
+
+                    ))}
             </div>
         </div>
 
